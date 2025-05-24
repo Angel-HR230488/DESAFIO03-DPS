dfa0a0cd9f9ad0b097d74b24c92b0c11cae1ecd3
TechSavvy-2025 <LB240048@alumno.udb.edu.sv>
2025-05-24 00:54:46 -0600

    Corrigiendo repositorio anidado

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
--- services/firebaseConfig.js
+++ services/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyB-OTEx4LAu8LT4X98c99o0XarRLiJ-p1I",
+  authDomain: "desafio03-b5910.firebaseapp.com",
+  projectId: "desafio03-b5910",
+  storageBucket: "desafio03-b5910.firebasestorage.app",
+  messagingSenderId: "979034825061",
+  appId: "1:979034825061:web:0e7b6556241a5b1597018c",
+  measurementId: "G-QGJD85NVE8"
 };
-
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
